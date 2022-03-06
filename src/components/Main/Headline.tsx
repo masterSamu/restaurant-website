28d93e8cbc0b11066674a81295acbbcc72ec29fe
Samu <samu.uunonen@student.hamk.fi>
2022-03-06 17:45:59 +0200

    main section of the page
    Main section UI is done.

---
+++ src/components/Main/Headline.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+export default function Headline() {
+  return (
+    <Container className="headline-container">
+        <h1>Pai <br></br>Streetfood</h1>
+        <p>Pirkanmaan paras katuruoka paikka</p>
+    </Container>
+  )
+}
