1bc4bfa5f4a7aedf55b7f7c12751262324246f63
Samu <samu.uunonen@student.hamk.fi>
2022-03-10 19:40:49 +0200

    opening-status
    Added link to google map location,
    finalized styling.

--- src/components/ShopStatus/ShopStatus.tsx
+++ src/components/ShopStatus/ShopStatus.tsx
@@ -1,33 +1,53 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import useFirestore from "../../Hooks/useFirestore";
 import "./ShopStatus.css";
 
-export default function ShopStatus() {
-  const isOpen = useFirestore();
+interface Props {
+  currentShopStatus:
+    | {
+        is_shop_open: boolean;
+        open_at: string;
+        close_at: string;
+        location: string;
+        location_url: string;
+      }
+    | undefined;
+}
+
+export default function ShopStatus({ currentShopStatus }: Props) {
   const [className, setClassName] = useState<string>("");
 
   useEffect(() => {
-    if (isOpen) {
-        setClassName("open");
+    if (currentShopStatus?.is_shop_open) {
+      setClassName("open");
     } else {
-        setClassName("closed");
+      setClassName("closed");
     }
-  }, [isOpen]);
+  }, [currentShopStatus?.is_shop_open]);
 
   return (
     <Container className={"shop-status-container " + className}>
-        <span className="shop-container-headline">Tänään</span>
-      {isOpen ? (
+      <span className="shop-container-headline">Tänään</span>
+      {currentShopStatus?.is_shop_open ? (
         <>
           <div className="shop-status-opening">
-            <span><b>Avoinna</b></span>
+            <span>
+              <b>Avoinna</b>
+            </span>
             <span className={"shop-status-icon " + className}>Avoinna</span>
-            <span>10:00 - 20:00</span>
+            <span>
+              {currentShopStatus?.open_at} - {currentShopStatus?.close_at}
+            </span>
           </div>
           <div className="shop-status-location">
-            <span><b>Sijainti</b></span>
-            <span>Nokia, Pirkkalaistori</span>
+            <span>
+              <b>Sijainti</b>
+            </span>
+            <span>
+              <a href={currentShopStatus?.location_url} target="_blank" >
+                {currentShopStatus?.location}
+              </a>
+            </span>
           </div>
         </>
       ) : (
@@ -38,7 +58,7 @@ export default function ShopStatus() {
           </div>
           <div className="shop-status-location">
             <span>
-              Katso huomisen sijainti <a href="#SCHEDULE">aikataulusta</a>
+              Katso huomisen sijainti ja <a href="#SCHEDULE">aikataulu</a>
             </span>
           </div>
         </>
